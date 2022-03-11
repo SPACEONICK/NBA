61387bb7d127889edea7e3408e8d2579375ff072
ONick Space <nickknyazev2001@gmail.com>
2022-03-11 03:33:51 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/admin', function(req, res, next) {
+  res.render('admin', );
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/teams', function(req, res, next) {
+  res.render('teams', );
+});
+
+module.exports = router;
